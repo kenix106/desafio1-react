88f54302d23349fa68557b8bca71aeb0e6b2bbeb
Kenny <meneses.kenneth@gmail.co>
2022-06-10 17:32:42 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+       <h1>
+        Desafio n° 1 - Coder - Kenneth Meneses
+       </h1>
+        <a
+        
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h3> Learn React </h3>
+         
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

15f3230cc4ac84e2c0cd4b5aaeeb8e96202bf33a
Kenny <meneses.kenneth@gmail.co>
2022-06-14 21:24:59 -0400

    Navbar3clase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       
-       <h1>
-        Desafio n° 1 - Coder - Kenneth Meneses
-       </h1>
-        <a
-        
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h3> Learn React </h3>
-         
-        </a>
-      </header>
+    <div className='Navbar'>
+          <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+
+import './Navbar.css';
+
+
+function Navbar() {
+  return (
+    <header>
+          <button>Home</button>
+          <p>Nosotros</p>
+          <a src="#">Tienda</a>
+    </header>
+  );
+}
+
+export default Navbar;
